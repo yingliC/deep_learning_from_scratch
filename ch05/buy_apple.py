3f1b16f5150a5e73c331c4abcfae47bdcac7f6fb
ZoraPea <dawnpea@outlook.com>
2019-08-31 21:17:57 +0800

    layers and simple computeGraph

---
+++ ch05/buy_apple.py
@@ -0,0 +1,20 @@
+from layer_naive import *
+
+apple = 100
+apple_num = 2
+tax = 1.1
+
+#layer
+mul_apple_layer = MulLayer()
+mul_tax_layer = MulLayer()
+
+#forward
+apple_price = mul_apple_layer.forward(apple, apple_num)
+price = mul_tax_layer.forward(apple_price, tax)
+print(price)
+
+#backward
+dprice = 1
+dapple_price, dtax = mul_tax_layer.backwoard(dprice)
+dapple, dapple_num = mul_apple_layer.backwoard(dapple_price)
+print(dapple, dapple_num, dtax)
